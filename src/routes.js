22113b3aa83faa1fda7de816800c42704cfd1aba
JoaoLuizMelo <joaoluiz.civil@gmail.com>
2023-10-24 20:49:55 -0300

    Criação do controler user e correção ro roter

--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,8 @@
 import { Router } from 'express'
-import { v4 } from 'uuid'
-import User from './app/models/User'
 
+import UserControler from './app/controllers/UserControler'
 const routes = new Router()
 
-routes.get('/', async (request, response) => {
-  const user = await User.create({
-    id: v4(),
-    name: 'José',
-    email: 'jose@hot.com',
-    password_hash: 'jurubeba2',
-  })
-  return response.json(user)
-})
+routes.post('/users', UserControler.store)
 
 export default routes
