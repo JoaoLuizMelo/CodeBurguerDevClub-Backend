db61686c19b4366b2594715009292e14a103c0cf
JoaoLuizMelo <joaoluiz.civil@gmail.com>
2023-11-02 11:30:29 -0300

    Controler done

--- src/app/controllers/UserControler.js
+++ src/app/controllers/UserControler.js
@@ -4,6 +4,7 @@ import User from '../models/User'
 
 class UserControler {
   async store(request, response) {
+    /* Padrão do objeto que o Yup espera receber para verificar as informações. */
     const schema = Yup.object().shape({
       name: Yup.string().required(),
       email: Yup.string().email().required(),
@@ -11,8 +12,17 @@ class UserControler {
       admin: Yup.boolean(),
     })
 
-    /* await schema.isValid(request.body)
+    /*
+    1º metodo de avaliação do yup ,  contudo ele retorna apenas true/false mais não da pra saber onde é o erro ( tudo certo - true /
+    algum erro nas validações - false).
+
+    if(!(await schema.isValid(request.body)))
+    return response
+    .status(400)
+    .json({error : 'Make sure your data is corect?'})
+    
     const { name, email, password_hash, admin } = request.body
+    
     */
 
     try {
@@ -21,6 +31,23 @@ class UserControler {
       return response.status(400).json({ error: err.errors })
     }
 
+    const { name, email, password_hash, admin } = request.body
+
+    const userExists = await User.findOne({
+      where: { email },
+    })
+    if (userExists) {
+      return response.status(400).json({ error: 'User already exists' })
+    }
+    console.log(userExists)
+
+    /*
+    Nesse arquivo vai ser criado os dados logo tem que ser instalado o UUID v4 e importado para gerar o ID. 
+    
+    Foi utilizado o metodo Create para criar o objeto do  usuario. depois é chamado o model para gravar no banco de dados
+    para isso é necessario importar o User.js que está dentro da pasta módel.
+    
+    */
     const user = await User.create({
       id: v4(),
       name,
@@ -28,6 +55,16 @@ class UserControler {
       password_hash,
       admin,
     })
+
+    /*
+    Se lembrar de sempre utilizar o codigo HTTP correto então: Alguns codigos. Consultar :
+    (https://developer.mozilla.org/pt-BR/docs/Web/HTTP/Status)
+
+    200 OK , 201 Created , 202 Accepted , 203 Non-Authoritative Information , 204 No Content ,205 Reset Content
+    400 Bad Request , 401 Unauthorized, 402 Payment Required, 403 Forbidden, 404 Not Found
+    500 Internal Server Error , 501 Not Implemented, 502 Bad Gateway
+
+    */
     return response.status(201).json({ id: user.id, name, email, admin })
   }
 }
