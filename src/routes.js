435d41d81f68cf790a7d90e0cafdb7691461a9f4
JoaoLuizMelo <125713134+JoaoLuizMelo@users.noreply.github.com>
2024-01-10 06:22:50 -0300

    finalizado a colocação do token JWT

--- src/routes.js
+++ src/routes.js
@@ -5,12 +5,16 @@ import SessionController from './app/controllers/SessionController'
 import ProductController from './app/controllers/ProductController'
 import multer from 'multer'
 import multerConfig from './config/multer'
+import authMiddleware from './app/middlewares/auth'
 
 const upload = multer(multerConfig)
 const routes = new Router()
 
 routes.post('/users', UserController.store)
 routes.post('/session', SessionController.store)
+
+routes.use(authMiddleware) // Será chamado o middleware antes das rotas de produtos.
+
 routes.post('/products', upload.single('file'), ProductController.store)
 routes.get('/products', ProductController.index)
 
